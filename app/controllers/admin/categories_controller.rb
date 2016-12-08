db15d6512ca24b2717b1a2af3f51de3e27296316
Adam <adam.dipinto@gmail.com>
2016-12-08 20:33:08 +0000

    Completed Admin Categories. Generated controller for admin/categories. Added methods: new, show, update, and create. Created views for index and new for categories.

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,33 @@
+class Admin::CategoriesController < ApplicationController
+
+  def index
+    @categories = Category.order(id: :desc).all
+  end
+
+  def show
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    respond_to do |format|
+      if @category.save
+        format.html { redirect_to @category, notice: 'Category was successfully created.'}
+        format.json { render :show, status: :created, location: @category }
+      else
+        format.html { render :new }
+        format.json { render json: @category.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    def category_params
+      params.fetch(:category, {}).permit(:name)
+    end
+
+end
